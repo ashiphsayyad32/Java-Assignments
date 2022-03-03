a4352303480b933eefa7556ec25b5e873e0f6e20
Ashiph <ashiphs@cybage.com>
2022-03-03 18:40:45 +0530

    Workshop for Core Java Completed

---
+++ ProductManagementSystem/src/main/java/JDBCUtility/JDBCUtility.java
@@ -0,0 +1,24 @@
+package JDBCUtility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtility {
+
+	private static Connection con=null;
+	
+	public static Connection getConnection() {
+		
+		
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demodb","root","Ashiph3283#");
+			System.out.println("Connected to Database!!!");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+}
